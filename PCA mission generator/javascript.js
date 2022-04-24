057761c6e2137cb5869b1ca8b800b2bad9edc1ce
cwylo <67430828+cwylo@users.noreply.github.com>
2022-04-24 10:25:21 -0700

    Updated to add a Serious Mission Generator

--- PCA mission generator/javascript.js
+++ PCA mission generator/javascript.js
@@ -24,12 +24,13 @@ var terrainTypes = [
 ];
 
 var objectiveTypes = [
-    "take and hold a town",
+    "take and hold a site",
     "rescue hostages",
     "retrieve intel",
     "destroy assets",
     "eliminate an HVT",
     "stage an ambush",
+    "take out enemy artillery"
 ];
 
 var gimmickTypes = [
@@ -48,14 +49,24 @@ var gimmickTypes = [
     "the players are getting mortared"
 ];
 
+var constraintTypes = [
+    "the mission takes place on only 1km x 1km of terrain",
+    "players must fight in low-visibility environments",
+    "key points throughout are heavily fortified by the enemy",
+    "players don't know the precise location of the objective",
+    "enemies are in good camouflage for the terrain",
+    "players have a restrictive ROE",
+    "all the obvious entry points are heavily fortified and patrolled"
+];
+
 function pickString(somelist) {
     let randomNumber = Math.floor(Math.random() * (somelist.length));
     return randomNumber;
 };
 
-function newMission() {
+function newSillyMission() {
 //Pick something for each array and store it.
-let missionArray = [
+let missionArraySilly = [
     bluforTypes[pickString(bluforTypes)],
     opforTypes[pickString(opforTypes)],
     terrainTypes[pickString(terrainTypes)],
@@ -64,6 +75,23 @@ let missionArray = [
 ];
 
 //Display it as a formatted HTML block.
-    document.getElementById('missionDisplay').innerHTML =  `<p>Players are a <em>${missionArray[0]}</em>, fighting <em>${missionArray[1]}</em>. This takes place on <em>${missionArray[2]}</em> terrain. Their main objective is to <em>${missionArray[3]}</em>. This mission\'s gimmick is that <em>${missionArray[4]}</em>.`;
+    document.getElementById('missionDisplaySilly').innerHTML =  `<p>Players are a <em>${missionArraySilly[0]}</em>, fighting <em>${missionArraySilly[1]}</em>. This takes place on <em>${missionArraySilly[2]}</em> terrain. Their main objective is to <em>${missionArraySilly[3]}</em>. This mission\'s gimmick is that <em>${missionArraySilly[4]}</em>.`;
 
 };
+
+//Duplicate, but for serious mission types.
+
+function newSrsMission() {
+    //Pick something for each array and store it.
+    let missionArraySrs = [
+        bluforTypes[pickString(bluforTypes)],
+        opforTypes[pickString(opforTypes)],
+        terrainTypes[pickString(terrainTypes)],
+        objectiveTypes[pickString(objectiveTypes)],
+        constraintTypes[pickString(constraintTypes)],
+    ];
+    
+    //Display it as a formatted HTML block.
+        document.getElementById('missionDisplaySrs').innerHTML =  `<p>Players are a <em>${missionArraySrs[0]}</em>, fighting <em>${missionArraySrs[1]}</em>. This takes place on <em>${missionArraySrs[2]}</em> terrain. Their main objective is to <em>${missionArraySrs[3]}</em>. This mission\'s constraint is that <em>${missionArraySrs[4]}</em>.`;
+    
+    };
